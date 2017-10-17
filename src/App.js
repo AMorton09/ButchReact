b2cc7c6c3d87489453b6858804f1c1d3cac4fe3a
Andrew Morton <morton09@me.com>
2017-10-17 11:58:22 -0500

    timeline added css is off

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Timeline } from 'react-twitter-widgets'
 import butchie from './butch.gif';
 import butchieNo from './ButchNope.png';
 import './App.css';
@@ -6,15 +7,29 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
+            <Timeline className="time-line"
+                      dataSource={{
+                          sourceType: 'widget',
+                          widgetId: '920322601217593345'
+                      }}
+                      options={{
+                          username: '',
+                          height: '600'
+                      }}
+                      onLoad={() => console.log('Timeline is loaded!')}
+            />
           <img src={butchie} className="App-logo" alt="logo" />
             <h1 className="App-title"><img src={butchieNo}/> </h1>
+
         </header>
         <p className="App-intro">
 
         </p>
       </div>
+
     );
   }
 }
